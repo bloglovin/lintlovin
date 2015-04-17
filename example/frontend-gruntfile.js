1ff5a856d6a5ff1dbe803402efb9ee8871963a7d
Pelle Wessman <pelle@kodfabrik.se>
2015-04-17 10:48:17 +0200

    docs(example): added comments to the frontend conf

--- example/frontend-gruntfile.js
+++ example/frontend-gruntfile.js
@@ -4,6 +4,20 @@
 var lintlovin = require('lintlovin');
 
 // A live-reloaded node-sass compiling live-reloading development server on top of Lintlovin
+
+// Folders:
+//   js/       – the frontend js
+//   css/      – the compiled frontend css
+//   example/  – the example HTML to use with the built in development server
+//   src/scss/ – the sass source files
+//   + any other top level folder will also be reachable by the built in server
+
+// Start the built in server with automated watching, linting and compiling by running: "grunt server"
+
+// Be sure to install these modules as dev dependencies in addition to Lintlovin:
+//   grunt-contrib-connect
+//   grunt-sass
+
 module.exports = function (grunt) {
   lintlovin.initConfig(grunt, {
     connect: {
